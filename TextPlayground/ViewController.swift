0827b846c455a28fdca372aa9289e7c0b1c4db59
10.12 <a10.12@1012demacbook-air.local>
2018-07-26 13:41:46 +0800

    Initial Commit

---
+++ TextPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextPlayground
+//
+//  Created by 10.12 on 2018/7/26.
+//  Copyright © 2018 10.12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
